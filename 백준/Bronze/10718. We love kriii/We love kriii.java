bdd9d80fa16c7f962936d5a177f366e418643500
yebin <122111459+benniejung@users.noreply.github.com>
2024-03-21 17:24:44 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+    private StringBuilder sb = new StringBuilder();
+
+    public void run() throws Exception {
+        sb.append("강한친구 대한육군");
+        System.out.println(sb);
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().run();
+    }
+}
