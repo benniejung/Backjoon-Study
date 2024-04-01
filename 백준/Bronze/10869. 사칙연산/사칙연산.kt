d79a4886d75694cd37b89d344fbe3bf786f8e8ed
yebin <122111459+benniejung@users.noreply.github.com>
2024-04-01 15:27:25 +0900

    [Bronze V] Title: 사칙연산, Time: 88 ms, Memory: 12208 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main(args: Array<String>){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+
+}
