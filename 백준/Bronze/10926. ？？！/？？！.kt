8d23453136f53f22d48ddaeff035e9015e2cd248
yebin <122111459+benniejung@users.noreply.github.com>
2024-04-01 15:34:50 +0900

    [Bronze V] Title: ??!, Time: 88 ms, Memory: 12212 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main(args: Array<String>){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val input:String = br.readLine()
+    println("${input}??!")
+}
