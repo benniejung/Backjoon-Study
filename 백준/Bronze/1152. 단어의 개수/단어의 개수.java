5e11c1da08510140769325fcc464475b5ac101bb
yebin <122111459+benniejung@users.noreply.github.com>
2024-04-02 19:11:33 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 23856 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -3,15 +3,21 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.*;
 
+// StringTokenizer클래스의 countTokens 사용하지 않는 다른 방법
 public class Main {
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private StringTokenizer st;
+//    private StringBuilder sb = new StringBuilder();
 
     public void run() throws Exception {
-        String str;
-        str = br.readLine();
-        st = new StringTokenizer(str, " "); // 객체 생성
-        System.out.println(st.countTokens());
+        st = new StringTokenizer(br.readLine(), " "); // 입력받은 문자열을 공백으로 나누는 StringTokenizer 생성
+        int cnt = 0;
+        while(st.hasMoreTokens()) {
+            st.nextToken(); // 다음 토큰으로 이동
+            cnt++;
+        }
+        System.out.println(cnt);
+
     }
     public static void main(String[] args) throws Exception{
         new Main().run();
