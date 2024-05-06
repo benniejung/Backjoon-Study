f2e3274959c003b67b0c6f4afa8c9b3345b5cf1e
yebin <122111459+benniejung@users.noreply.github.com>
2024-05-07 01:16:48 +0900

    [Bronze II] Title: 16진수, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public void run() throws Exception {
+        int a = Integer.parseInt(br.readLine(), 16);
+        System.out.println(a);
+
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().run();
+    }
+}
