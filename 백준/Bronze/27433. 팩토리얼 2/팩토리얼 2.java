20d41122e39100b375eaa7c2f80deb1af64ec9d2
yebin <122111459+benniejung@users.noreply.github.com>
2024-06-08 21:45:36 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		if (N < 0 || N > 20)
+			return;
+
+		System.out.println(getFactorial(N));
+
+	}
+
+	public static long getFactorial(int num) {
+
+		return num == 0 || num == 1 ? 1 : num * getFactorial(num - 1);
+	}
+}
