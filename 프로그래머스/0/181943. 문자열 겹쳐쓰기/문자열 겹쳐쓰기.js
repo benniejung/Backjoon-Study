c3798b8bcdefc5e982023e70dba46232b7632a47
yebin <122111459+benniejung@users.noreply.github.com>
2026-05-02 15:57:51 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 3.93 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,9 +1,6 @@
 function solution(my_string, overwrite_string, s) {
-    var answer = '';
-    const length = s + overwrite_string.length;
-    var word = my_string.substring(0, s);
-    var word2 = my_string.substring(length);
-    answer = word + overwrite_string + word2;
+    var answer = my_string.split('');
+    answer.splice(s, overwrite_string.length, overwrite_string);
     console.log(answer);
-    return answer;
+    return answer.join('');
 }
